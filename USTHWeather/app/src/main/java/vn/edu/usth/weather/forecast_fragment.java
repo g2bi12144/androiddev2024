6aeda73d622bb172902196db8a59e53f59aab842
Bui Ngoc Hai BI12-144 <haibn.bi12-144@st.usth.edu.vn>
2024-09-29 00:02:14 +0700

    practical4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/forecast_fragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/forecast_fragment.java
@@ -5,14 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 
 public class forecast_fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = new View(getActivity());
-        view.setBackgroundColor(Color.parseColor("#20FF0000")); // Example color
-        return view;
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView textView = new TextView(getActivity());
+        textView.setText("Thursday");
+        textView.setTextSize(24);
+        textView.setTextColor(Color.BLACK);
+
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageResource(R.drawable.weather_icon); // Replace with your icon name
+
+        layout.addView(textView);
+        layout.addView(imageView);
+
+        return layout;
     }
 }
