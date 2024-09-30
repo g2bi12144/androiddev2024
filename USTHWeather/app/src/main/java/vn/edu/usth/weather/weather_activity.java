89d5de479ffaf85d6fba98e1cafcb84cdc055940
Bui Ngoc Hai BI12-144 <haibn.bi12-144@st.usth.edu.vn>
2024-09-30 21:44:41 +0700

    practical8

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/weather_activity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/weather_activity.java
@@ -1,31 +1,33 @@
 package vn.edu.usth.weather;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class weather_activity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private weather_adapter adapter;
+    private TabLayout tabLayout;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_activity);
 
         viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+
         adapter = new weather_adapter(getSupportFragmentManager(), createFragments());
         viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        setupTabTitles();
     }
 
     private List<Fragment> createFragments() {
@@ -34,5 +36,11 @@ public class weather_activity extends AppCompatActivity {
             fragments.add(new weather_and_forecast_fragment());
         }
         return fragments;
+    }
+
+    private void setupTabTitles() {
+        tabLayout.getTabAt(0).setText("Hanoi");
+        tabLayout.getTabAt(1).setText("Paris");
+        tabLayout.getTabAt(2).setText("Toulouse");
     }
 }
